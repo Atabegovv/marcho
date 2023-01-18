1e193723e969134021050b022c7c3ae0d547191c
Atabegovv <atabegov98@mail.ru>
2023-01-18 04:20:36 +0300

    slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,14 @@
 $(function () {
 
-  
+  $('.slider').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
   
 });
